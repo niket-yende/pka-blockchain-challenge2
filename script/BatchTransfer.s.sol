4eafad96bf207604010e8dfc2c41ac7dbaf668af
Niket Yende <niket6@gmail.com>
2024-02-03 20:01:30 +0530

    Update deploy script for BatchTransfer contracrt

--- script/BatchTransfer.s.sol
+++ script/BatchTransfer.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 import {Script, console2} from "forge-std/Script.sol";
 import {BatchTransfer} from "../src/BatchTransfer.sol";
 import {MyToken} from "../src/MyToken.sol";
+import "forge-std/console.sol";
 
 contract BatchTransferScript is Script {
     uint64 public constant batchLimit = 10;
@@ -13,6 +14,7 @@ contract BatchTransferScript is Script {
         // Deploy the dummy ERC20 token
         MyToken myToken = new MyToken();
         BatchTransfer batchTransfer = new BatchTransfer(address(myToken), batchLimit);
+        console.log('Deployed BatchTransfer address: ', address(batchTransfer));
         // mint the tokens for BatchTransfer contract
         // myToken.mint(address(batchTransfer), 10);
     }
