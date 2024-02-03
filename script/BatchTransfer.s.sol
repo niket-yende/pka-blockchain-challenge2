8e6a86108d95fbdd7b1b9748269dfd4be8369f7a
Niket Yende <niket6@gmail.com>
2024-02-03 16:31:38 +0530

    Add BatchTransfer deploy script

--- script/BatchTransfer.s.sol
+++ script/BatchTransfer.s.sol
@@ -6,16 +6,13 @@ import {BatchTransfer} from "../src/BatchTransfer.sol";
 import {MyToken} from "../src/MyToken.sol";
 
 contract BatchTransferScript is Script {
-    uint256 public constant totalSupply = 10000;
-
-    // function setUp() public {}
-
+    
     function run() public {
         vm.broadcast();
         // Deploy the dummy ERC20 token
         MyToken myToken = new MyToken();
         BatchTransfer batchTransfer = new BatchTransfer(address(myToken));
         // mint the tokens for BatchTransfer contract
-        myToken.mint(address(batchTransfer), totalSupply);
+        // myToken.mint(address(batchTransfer), 10000);
     }
 }
